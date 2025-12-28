23e145a552333c9d589a8a50523f6634fddad68d
Yash Vinayak Jadhav <129679321+YashJadhav100@users.noreply.github.com>
2025-12-28 00:15:51 -0500

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,9 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="banking_db",
-        user="postgres",
-        password="YASH"
+        host=("DB_HOST"),
+        database=("DB_NAME"),
+        user=("DB_USER"),
+        password=("DB_PASSWORD"),
+        port=("DB_PORT")
     )
